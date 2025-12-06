94219c21a7b950bd7e5c7e4093b92e8d9b1ce735
sahaj-b <sahajb0606@gmail.com>
2025-12-06 21:12:56 +0530

    use env var for server url

--- frontend/src/const.ts
+++ frontend/src/const.ts
@@ -1,5 +1,4 @@
-// export const SERVER_URL = "http://192.168.0.169:4000";
-// export const API_URL = "http://192.168.0.169:4000/api";
-export const SERVER_URL = "http://localhost:4000";
-export const API_URL = "http://localhost:4000/api";
+export const SERVER_URL =
+	process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:4000";
+export const API_URL = `${SERVER_URL}/api`;
 export const DEFAULT_KEY_BUFFER_SIZE = 1;
